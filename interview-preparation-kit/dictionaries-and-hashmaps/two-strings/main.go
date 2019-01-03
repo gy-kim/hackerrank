e998879ce6b5a5d99575108bb52ae38d05b7eea0
Geunyoung Kim <kgysmart@gmail.com>
2019-01-03 15:34:30 -0800

    Add two strings

--- interview-preparation-kit/dictionaries-and-hashmaps/two-strings/main.go
+++ interview-preparation-kit/dictionaries-and-hashmaps/two-strings/main.go
@@ -6,27 +6,42 @@ import (
 
 // Complete the twoStrings function below.
 func twoStrings(s1 string, s2 string) string {
-	skip := map[rune]struct{}{}
-	for _, s := range s1 {
-		if _, ok := skip[s]; ok {
-			continue
-		}
-		for _, t := range s2 {
-			if s == t {
-				return "YES"
+	/*
+		skip := map[rune]struct{}{}
+		for _, s := range s1 {
+			if _, ok := skip[s]; ok {
+				continue
 			}
+			for _, t := range s2 {
+				if s == t {
+					return "YES"
+				}
+			}
+			skip[s] = struct{}{}
 		}
-		skip[s] = struct{}{}
+		return "NO"
+	*/
+
+	m := make(map[rune]bool, len(s1))
+	for _, s := range s1 {
+		m[s] = true
 	}
+
+	for _, s := range s2 {
+		if _, ok := m[s]; ok {
+			return "YES"
+		}
+	}
+
 	return "NO"
 }
 
 func main() {
-	s1 := "hello"
-	s2 := "world"
-
-	// s1 := "h1"
+	// s1 := "hello"
 	// s2 := "world"
+
+	s1 := "h1"
+	s2 := "world"
 
 	fmt.Println(twoStrings(s1, s2))
 }
